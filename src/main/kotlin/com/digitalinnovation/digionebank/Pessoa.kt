cef92bdc936e5357a7d45256d71140dfe6113d6c
Júnior Moura <geraldomartins10@gmail.com>
2022-06-28 14:12:29 -0300

    modificação do modificador de acesso na propriedade cpf para private

--- src/main/kotlin/com/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/com/digitalinnovation/digionebank/Pessoa.kt
@@ -4,4 +4,17 @@ class Pessoa {
 
     var nome: String = "Júnior"
     var cpf: String = "123.456.789-10"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
+
+}
+
+fun main() {
+    val junior = Pessoa()
+
+    println(junior.pessoaInfo())
+
 }
