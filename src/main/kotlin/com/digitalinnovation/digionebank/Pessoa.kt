bf2aeff099c3bb6a70e9e037565e8f0829c875a5
Júnior Moura <geraldomartins10@gmail.com>
2022-06-28 14:13:38 -0300

    criação do método construtor (secundário) e da função pessoaInfo.

--- src/main/kotlin/com/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/com/digitalinnovation/digionebank/Pessoa.kt
@@ -2,7 +2,7 @@ package com.digitalinnovation.digionebank
 
 class Pessoa {
 
-    var nome: String = "Júnior"
+    var nome: String = "Junior"
     var cpf: String = "123.456.789-10"
     private set
 
